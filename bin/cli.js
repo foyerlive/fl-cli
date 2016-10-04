f522de87a30fe9b31d445dd3e10e26f7c362257b
James Turle <james@foyerlive.com>
2016-10-03 21:09:14 -0400

    ok

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env babel-node
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var publish = function () {
   var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(env) {
     var authResult, file, packageContents, packageObject, fileContents, form, headers, host;
@@ -119,16 +121,24 @@ var fs = require('fs');
 var exec = require('child_process').execSync;
 var execFile = require('child_process').execFileSync;
 
-_commander2.default.version('0.0.1').option('-s, --start', 'Start developer environment').option('-b, --build', 'Build for production environment').option('-p, --publish', 'Publish application').option('-e, --env [env]', 'Environment override', 'prod').option('-p, --port [port]', 'Override the development server port', 9081).parse(process.argv);
+_commander2.default.version('1.0.2').option('-s, --start', 'Start developer environment').option('-t, --theme', 'Theme developer mode').option('-b, --build', 'Build for production environment').option('-p, --publish', 'Publish application').option('-e, --env [env]', 'Environment override', 'prod').option('-p, --port [port]', 'Override the development server port', 9081).parse(process.argv);
 
 console.log('FoyerLive CLI: ' + _commander2.default.version());
-if (_commander2.default.start) {
-  // Run the development environment
-  if (_commander2.default.port !== 9081) process.env.FLDEVPORT = _commander2.default.port;
 
+// Port shift...
+if (_commander2.default.port !== 9081) process.env.FLDEVPORT = _commander2.default.port;
+
+// Get environment
+var env = process.env;
+
+// Let theme mode pass a environment variable to the webpack config...
+if (_commander2.default.theme) env = _extends({}, env, { foyerThemeMode: true, foyerDevelopmentPort: 9082 });
+
+// Run the development environment
+if (_commander2.default.start) {
   execFile('./node_modules/fl-cli/lib/devServer.js', [], {
     stdio: 'inherit',
-    env: process.env
+    env: env
   });
 }
 if (_commander2.default.build) {
@@ -137,7 +147,8 @@ if (_commander2.default.build) {
   });
 
   exec('NODE_ENV=production ./node_modules/.bin/webpack --config ./node_modules/fl-cli/lib/config/webpack.config.prod.js', {
-    stdio: 'inherit'
+    stdio: 'inherit',
+    env: env
   });
 }
 if (_commander2.default.publish) {
