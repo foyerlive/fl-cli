9d62e1d208f12eb72781e4506afa1eacc773e1d2
James Turle <james@foyerlive.com>
2017-04-04 17:08:05 -0400

    Added a bunch more memory to the build process...

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -4,26 +4,29 @@ require('es6-promise').polyfill();
 const exec = require('child_process').execSync;
 const execFile = require('child_process').execFileSync;
 
+var fs = require('fs');
+var packageContents = fs.readFileSync('./node_modules/fl-cli/package.json', 'utf8');
+var packageObject = JSON.parse(packageContents);
+
 import publish from '../lib/publish';
 
 import program from 'commander';
 
-
 program
-  .version('1.0.2')
-  .option('-s, --start', 'Start developer environment')
-  .option('-t, --theme', 'Theme developer mode')
-  .option('-b, --build', 'Build for production environment')
-  .option('-p, --publish', 'Publish application')
-  .option('-e, --env [env]', 'Environment override', 'prod')
-  .option('-p, --port [port]', 'Override the development server port (Not available for themes)', 9081)
-  .parse(process.argv);
+.version(packageObject.version)
+.option('-s, --start', 'Start developer environment')
+.option('-t, --theme', 'Theme developer mode')
+.option('-b, --build', 'Build for production environment', false)
+.option('-p, --publish', 'Publish application')
+.option('-e, --env [env]', 'Environment override', 'prod')
+.option('-p, --port [port]', 'Override the development server port (Not available for themes)', 9081)
+.option('-c, --config [config]', 'Override the build config', './node_modules/fl-cli/lib/config/webpack.config.prod.js')
+.parse(process.argv);
 
 console.log('FoyerLive CLI: ' + program.version());
 
 // Port shift...
-if (program.port !== 9081)
-{
+if (program.port !== 9081) {
   process.env.FLDEVPORT = program.port;
 }
 
@@ -31,8 +34,10 @@ if (program.port !== 9081)
 let env = process.env;
 
 // Let theme mode pass a environment variable to the webpack config...
-if (program.theme)
+if (program.theme) {
+  console.log('Theme mode enabled...');
   env = {...env, foyerThemeMode: true, FLDEVPORT: 9082};
+}
 
 // Run the development environment
 if (program.start) {
@@ -41,16 +46,21 @@ if (program.start) {
     env: env
   });
 }
+
+// Run the build...
 if (program.build) {
   exec('./node_modules/.bin/eslint src/ && ./node_modules/.bin/rimraf dist', {
     stdio: 'inherit'
   });
-
-  exec('NODE_ENV=production ./node_modules/.bin/webpack --config ./node_modules/fl-cli/lib/config/webpack.config.prod.js', {
+  console.log('Build config:', program.config);
+  console.log('Memory: 4096');
+  exec('NODE_ENV=production node --max_old_space_size=4096 ./node_modules/.bin/webpack --config ' + program.config, {
     stdio: 'inherit',
     env: env
   });
 }
+
+// Run a publish...
 if (program.publish) {
   publish(program.env).catch((err) => {
     console.error(err);
